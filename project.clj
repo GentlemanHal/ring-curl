6cfabed9f276d4ec448cf7b31cf47b1e0babe606
Christopher Martin <cmartin@thoughtworks.com>
2014-08-19 13:05:02 +0100

    adds midje as a test dependency

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [camel-snake-kebab "0.2.2"]
-                 [midje "1.6.3"]]
-  :profiles {:dev {:plugins [[lein-midje "3.1.3"]]}})
+                 [camel-snake-kebab "0.2.2"]]
+  :profiles {:dev {:plugins [[lein-midje "3.1.3"]]
+                   :dependencies [[midje "1.6.3"]]}})
