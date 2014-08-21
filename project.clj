2c97b5db23268daca29333016ada2ccbd72dfeba
Christopher Martin <cmartin@thoughtworks.com>
2014-08-21 23:09:18 +0100

    moves clojure core to dev dependency

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject ring-curl "0.1.0-SNAPSHOT"
-  :description "Converts ring requests to cURL commands"
-  :url "https://github.com/GentlemanHal/ring-curl"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [camel-snake-kebab "0.2.2"]]
-  :profiles {:dev {:plugins [[lein-midje "3.1.3"]]
-                   :dependencies [[midje "1.6.3"]]}})
+            :description "Converts ring requests to cURL commands"
+            :url "https://github.com/GentlemanHal/ring-curl"
+            :license {:name "Eclipse Public License"
+                      :url  "http://www.eclipse.org/legal/epl-v10.html"}
+            :dependencies [[camel-snake-kebab "0.2.2"]]
+            :profiles {:dev {:plugins      [[lein-midje "3.1.3"]]
+                             :dependencies [[org.clojure/clojure "1.6.0"]
+                                            [midje "1.6.3"]]}})
