fb9b4c08a440afde7de8a3409da2711bac306af2
Christopher Martin <cmartin@thoughtworks.com>
2014-09-15 16:03:09 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,9 @@
             :url "https://github.com/GentlemanHal/ring-curl"
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[camel-snake-kebab "0.2.2" :exclusions [org.clojure/clojure]]
-                           [org.clojure/tools.logging "0.3.0"]]
+            :dependencies [[camel-snake-kebab "0.2.4"]
+                           [org.clojure/tools.logging "0.3.0"]
+                           [org.clojure/data.json "0.2.5"]]
             :profiles {:dev {:plugins      [[lein-midje "3.1.3"]
                                             [lein-ancient "0.5.5"]]
                              :dependencies [[org.clojure/clojure "1.6.0"]
