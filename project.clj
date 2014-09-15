235903a7c1159acc82df654a160bd09ead4afce6
Christopher Martin <cmartin@thoughtworks.com>
2014-09-15 15:38:23 +0100

    Exclude clojure from dep and add ancient plugin

--- project.clj
+++ project.clj
@@ -3,9 +3,10 @@
             :url "https://github.com/GentlemanHal/ring-curl"
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[camel-snake-kebab "0.2.2"]
+            :dependencies [[camel-snake-kebab "0.2.2" :exclusions [org.clojure/clojure]]
                            [org.clojure/tools.logging "0.3.0"]]
-            :profiles {:dev {:plugins      [[lein-midje "3.1.3"]]
+            :profiles {:dev {:plugins      [[lein-midje "3.1.3"]
+                                            [lein-ancient "0.5.5"]]
                              :dependencies [[org.clojure/clojure "1.6.0"]
                                             [midje "1.6.3"]
                                             [clj-http "1.0.0"]]}})
