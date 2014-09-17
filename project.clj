03f0bc6a4cb36ef3510a6b734e7d5406bf4b7bcf
Christopher Martin <cmartin@thoughtworks.com>
2014-09-17 10:56:47 +0100

    bumps the version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-curl "0.1.2"
+(defproject ring-curl "0.1.3"
             :description "Converts ring requests to cURL commands"
             :url "https://github.com/GentlemanHal/ring-curl"
             :license {:name "Eclipse Public License"
