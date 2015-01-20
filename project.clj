e265bd7e93c3c5e0ae06bf09ecdb27f89195d135
Christopher Martin <cmartin@thoughtworks.com>
2015-01-20 11:07:26 +0000

    update dependencies to latest

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject ring-curl "0.1.4"
+(defproject ring-curl "0.1.5"
             :description "Converts ring requests to cURL commands"
             :url "https://github.com/GentlemanHal/ring-curl"
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/tools.logging "0.3.0"]
+            :dependencies [[org.clojure/tools.logging "0.3.1"]
                            [org.clojure/data.json "0.2.5"]]
             :profiles {:dev {:plugins      [[lein-midje "3.1.3"]
                                             [lein-ancient "0.5.5"]]
                              :dependencies [[org.clojure/clojure "1.6.0"]
                                             [midje "1.6.3"]
-                                            [clj-http "1.0.0"]]}})
+                                            [clj-http "1.0.1"]]}})
