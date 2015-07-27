2de282523408f920460d969d68dc095e600b0d25
Christopher Martin <cmartin@thoughtworks.com>
2015-07-27 22:45:00 +0100

    updates libraries

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/tools.logging "0.3.1"]
-                           [org.clojure/data.json "0.2.5"]]
+                           [org.clojure/data.json "0.2.6"]]
             :profiles {:dev {:plugins      [[lein-midje "3.1.3"]
-                                            [lein-ancient "0.5.5"]]
+                                            [lein-ancient "0.6.7"]]
                              :dependencies [[org.clojure/clojure "1.6.0"]
-                                            [midje "1.6.3"]
-                                            [clj-http "1.0.1"]]}})
+                                            [midje "1.7.0"]
+                                            [clj-http "2.0.0"]]}})
