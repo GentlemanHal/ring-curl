471a1aae60b3895797a849843a9d4d3a1465d967
Christopher Martin <cmartin@thoughtworks.com>
2018-08-05 13:21:29 +0100

    store the built jar as an artifact

--- ci/compile.sh
+++ ci/compile.sh
@@ -5,3 +5,8 @@ echo "cleaning build folders"
 
 echo "building a jar"
 ./lein.sh jar
+
+echo "moving the built jar"
+# This is because CircleCI doesn't support wildcards when storing artifacts and the jar contains the version number
+mkdir target/jar
+mv target/*.jar target/jar/
