2467f3a25915da7f880f370972122116bedd8e65
Christopher Martin <cmartin@thoughtworks.com>
2018-08-05 13:55:25 +0100

    fixes the version name

--- ci/release-github.sh
+++ ci/release-github.sh
@@ -5,7 +5,7 @@ echo "Creating a GitHub release with tag [${VERSION}]"
 responseJson=$(curl \
   -u ${GITHUB_USERNAME}:${GITHUB_TOKEN} \
   -H "Content-Type: application/vnd.github.v3+json" \
-  -d "{\"tag_name\": \"v${VERSION}\", \"target_commitish\": \"master\", \"name\": \"${VERSION}\", \"draft\": true, \"prerelease\": false}" \
+  -d "{\"tag_name\": \"v${VERSION}\", \"target_commitish\": \"master\", \"name\": \"v${VERSION}\", \"draft\": true, \"prerelease\": false}" \
   https://api.github.com/repos/GentlemanHal/ring-curl/releases)
 
 echo "Got response [${responseJson}]"
